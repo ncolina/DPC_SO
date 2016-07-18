c4b22cb49b671c41765de90feb8d8ba2e746e3a0
Nicholas Colina <nicholascolina1@gmail.com>
2016-07-18 15:03:33 +0800

    Functions
    Initial definition of functions that will be used in the python script

---
+++ imports.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def get_database(file):
+    database=pd.read_hdf(file)
+    return  database
+
+def update_database(database)
+
+def build_database()
+
+def build_new_entries(file)
+
+def create_ouput_db()
+
+def create_ouput_crm()
